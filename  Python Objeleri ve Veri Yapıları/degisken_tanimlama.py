1ffcb9c7de44306e488ccd2f0dc10bb6ba50dee6
Kerim DELİL <kerimdelil@gmail.com>
2023-08-26 17:05:21 +0300

    değişken tanımlama nasıl yapılır anlatıldı.

---
+++  Python Objeleri ve Veri Yapıları/degisken_tanimlama.py
@@ -0,0 +1,33 @@
+# Python'da istenilen değerleri tutmak için değişkenler kullanılır. Bir değişkenin içinde sadece 1 tane değer bulunabilir. Bu tarz değişkenlere primity değişkenler denir.
+# KURALLAR
+# -> değişkenler rakam ile başlayamazlar. Ama başında "_" işareti gelebilir.
+
+# -> Türkçe karakterler kullanılmamalıdır.
+yaş = 20 # yerine 
+age = 20 #daha makbuldür. yada
+yas = 20 # de olabilir.
+
+# 2 veya daha fazla kelimeden oluşacak değişkenlerin arasına "_" işareti konulabileceği gibi daha çok camelCase yapısı kullanılır. Yani ilk kelimenin ilk harfi küçük, diğer kelimelerin ilk hafleri büyük yazılırlar.
+
+studentNumber = 1234
+personelId = 1
+productName = "Televizyon"
+
+# Python diline has olan komutlar değişken olarak kullanılamazlar.
+# for = 12 if = 23 vb.
+
+# Python'da BÜYÜK küçük harf duyarlılığı vardır.
+age = 20
+Age = 30
+AGE = 40
+print(age)
+print(Age)
+print(AGE)
+
+# TODO: NOT=> tek satırda birden fazla değişkene değer atamak için öncelikle değişkenler virgüllerle yazılır. Daha sonra eşittir ifadesi ile yine değerleri aralara virgül konularak yazılır.
+
+
+
+
+
+
---
+++  Python Objeleri ve Veri Yapıları/sayi_veri_tipleri.py
@@ -0,0 +1,6 @@
+# Pythonda sayı veri tipleri Integer ve Float'dır.
+x = 10
+y = 12.3
+print(x/y)
+
+
