2ad8e49b59d7aafc4a9201944de17b43524526aa
Kerim DELİL <kerimdelil@gmail.com>
2023-08-28 00:14:02 +0300

    atama operatörlerinin pekiştirilmesi için uygulama

---
+++ Python Operatörler/lesson2_uygulama_atama_operatorleri.py
@@ -0,0 +1,32 @@
+# Python'da atama operatörleri hakkında pekiştirme yapmak için uygulamalar
+x,y,z = 2,5,10
+numbers = 1,5,7,10,6
+
+# 1- Kullanıcıdan alınan 2 sayının çarpımı ile x,y,z değişkenlerinin toplamının farkını hesaplayınız.
+# number1 = int(input("1. sayı:"))
+# number2 = int(input("2. sayı:"))
+# result = (number1+number2) - (x+y+z)
+# print(result)
+# ----------------------------------------------------------------------------------------------------
+
+# 2- y'nin x'e kalansız bölümünü hesaplayınız.
+# print(y//x)
+# ----------------------------------------------------------------------------------------------------
+
+# 3- (x,y,z) toplamının mod3'ü nedir?,
+# print( (x+y+z)%3 )
+# ----------------------------------------------------------------------------------------------------
+
+# 4- y'nin x. kuvvetini hesaplayınız.
+# print(y**x)
+# ----------------------------------------------------------------------------------------------------
+
+# 5- x,*y,z = numbers işlemine göre z'nin küpü nedir?
+# x,*y,z = numbers
+# print(z**3)
+# ----------------------------------------------------------------------------------------------------
+
+# 6- x,*y,z = numbers işlemine göre y'nin değerleri toplamı nedir?
+# x,*y,z = numbers
+# result = y[0] + y[1] + y[2]
+# print(result)
